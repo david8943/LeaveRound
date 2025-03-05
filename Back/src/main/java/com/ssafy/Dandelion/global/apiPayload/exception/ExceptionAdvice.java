39b9094c4b838b79316f1a361b8a884829350197
TinyFrogs <qns0147@gmail.com>
2025-03-05 15:30:21 +0900

    Refactor: 불필요한 정규화 제거

--- Back/src/main/java/com/ssafy/Dandelion/global/apiPayload/exception/ExceptionAdvice.java
+++ Back/src/main/java/com/ssafy/Dandelion/global/apiPayload/exception/ExceptionAdvice.java
@@ -27,7 +27,7 @@ import java.util.Optional;
 public class ExceptionAdvice extends ResponseEntityExceptionHandler {
 
 
-    @org.springframework.web.bind.annotation.ExceptionHandler
+    @ExceptionHandler
     public ResponseEntity<Object> validation(ConstraintViolationException e, WebRequest request) {
         String errorMessage = e.getConstraintViolations().stream()
                 .map(constraintViolation -> constraintViolation.getMessage())
@@ -54,7 +54,7 @@ public class ExceptionAdvice extends ResponseEntityExceptionHandler {
         return handleExceptionInternalArgs(e,HttpHeaders.EMPTY,ErrorStatus.valueOf("_BAD_REQUEST"),request,errors);
     }
 
-    @org.springframework.web.bind.annotation.ExceptionHandler
+    @ExceptionHandler
     public ResponseEntity<Object> exception(Exception e, WebRequest request) {
         e.printStackTrace();
 
