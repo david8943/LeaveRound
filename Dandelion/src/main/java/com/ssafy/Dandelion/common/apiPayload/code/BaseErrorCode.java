521859721444c82543f2ad10326e55ae83c98ce1
juwon <juwon0404@nate.com>
2025-03-05 14:37:54 +0900

    전체 프로젝트 구조 추가

--- Dandelion/src/main/java/com/ssafy/Dandelion/common/apiPayload/code/BaseErrorCode.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.Dandelion.common.apiPayload.code;
-
-public interface BaseErrorCode {
-
-    public ErrorReasonDTO getReason();
-
-    public ErrorReasonDTO getReasonHttpStatus();
-}
