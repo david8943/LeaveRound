1aab7499296b8a3009052c34d0d14eefc2d0c5ce
Huisu <87214089+ranunclulus@users.noreply.github.com>
2025-03-16 13:05:08 +0900

    S12P21A303-46-Feat: SecurityConfig, JwtAuthenticationFilter, LoginFilter, LogoutFilter 작성
    구체적인 api endpoint나 login,logout 경로 공란으로 작성

--- Back/src/main/java/com/ssafy/Dandelion/global/apiPayload/code/status/ErrorStatus.java
+++ Back/src/main/java/com/ssafy/Dandelion/global/apiPayload/code/status/ErrorStatus.java
@@ -26,7 +26,8 @@ public enum ErrorStatus implements BaseErrorCode {
     // 인증 관련 에러
     EXPIRED_TOKEN(HttpStatus.FORBIDDEN, "AUTH4004", "유효 기간이 만료된 토큰입니다."),
     INVALID_TOKEN(HttpStatus.FORBIDDEN, "AUTH4005", "유효하지 않은 토큰입니다."),
-    UNAUTHORIZED_USER(HttpStatus.FORBIDDEN, "AUTH4006", "인가된 사용자가 아닙니다."),
+    UNAUTHORIZED(HttpStatus.FORBIDDEN, "AUTH4006", "인증된 사용자가 아닙니다."),
+    INVALID_CREDENTIALS(HttpStatus.FORBIDDEN, "AUTH4007", "잘못된 자격 증명입니다."),
 
     // Ror test
     TEMP_EXCEPTION(HttpStatus.BAD_REQUEST, "TEMP4001", "이거는 테스트");
