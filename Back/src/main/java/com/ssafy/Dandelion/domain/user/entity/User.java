97a958ab8caaedcdb77263302f0077dfce4bc2b5
Huisu <87214089+ranunclulus@users.noreply.github.com>
2025-03-16 12:55:37 +0900

    S12P21A303-46-Feat: CustomUserDetailService 작성과 엔티티 참조를 위해 User 테이블 Annotation 추가 -> 이후 논의 필요

--- Back/src/main/java/com/ssafy/Dandelion/domain/user/entity/User.java
+++ Back/src/main/java/com/ssafy/Dandelion/domain/user/entity/User.java
@@ -9,8 +9,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Table(name = "users")
 public class User extends BaseTimeEntity {
 
 	@Id
