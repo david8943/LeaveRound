5f7f1cfcd317b60ab5ed2f5606879db86055b775
이승희 <dearlsh1125@gmail.com>
2025-03-20 11:39:53 +0900

    S12P21A303-42-Chore: asset에 svg파일 추가

--- Front/LeaveRound/src/assets/aseets.tsx
+++ Front/LeaveRound/src/assets/aseets.tsx
@@ -1,4 +1,28 @@
 // account card
-import tossIcon from './icons/toss.svg';
+import tossIcon from '@/assets/icons/toss.svg';
+import brackets from '@/assets/icons/brackets.svg';
+import menuIcon from '@/assets/icons/menu.svg';
+import cardIcon from '@/assets/icons/card.svg';
+import dandelionIcon from '@/assets/icons/dandelion.svg';
 
-export { tossIcon };
+// menu navbar
+import homeIcon from '@/assets/icons/home.svg';
+import eventIcon from '@/assets/icons/event.svg';
+import donateIcon from '@/assets/icons/donate.svg';
+import homeActiveIcon from '@/assets/icons/home-click.svg';
+import eventActiveIcon from '@/assets/icons/event-click.svg';
+import donateActiveIcon from '@/assets/icons/donate-click.svg';
+
+export {
+  tossIcon,
+  brackets,
+  menuIcon,
+  cardIcon,
+  dandelionIcon,
+  homeIcon,
+  eventIcon,
+  donateIcon,
+  homeActiveIcon,
+  eventActiveIcon,
+  donateActiveIcon,
+};
