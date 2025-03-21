f7821a4b3c9228b26d56c0d6810fbc897327ef07
이승희 <dearlsh1125@gmail.com>
2025-03-21 17:06:20 +0900

    S12P21A303-46-Feat: 계좌 메뉴 드롭다운 구현

---
+++ Front/LeaveRound/src/components/Account/AccountMenu.tsx
@@ -0,0 +1,56 @@
+interface AccountMenuProps {
+  onClose: () => void;
+}
+
+export const AccountMenu = ({ onClose }: AccountMenuProps) => {
+  const handleDetail = () => {
+    console.log('상세 내역 페이지 이동');
+    onClose();
+  };
+
+  const handleModify = () => {
+    console.log('기부 수정 모달 열기');
+    onClose();
+  };
+
+  const handleStop = () => {
+    console.log('기부 중지 모달 열기');
+    onClose();
+  };
+
+  const handleDelete = () => {
+    console.log('기부 삭제 모달 열기');
+    onClose();
+  };
+
+  // 버튼 호버 공통 스타일 변수
+  const menuItemClass =
+    'w-full py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white transition-colors duration-200 rounded-lg';
+
+  return (
+    <div className='flex flex-col justify-center w-[6rem] rounded-lg bg-white bg-opacity-97 shadow-[0_0_4.5px_rgba(214,214,214,0.10)] z-50 '>
+      <ul className='flex flex-col items-center justify-center text-gray-700 text-detail'>
+        <li className='w-full'>
+          <button className={menuItemClass} onClick={handleDetail}>
+            상세 내역
+          </button>
+        </li>
+        <li className='w-full'>
+          <button className={menuItemClass} onClick={handleModify}>
+            기부 수정
+          </button>
+        </li>
+        <li className='w-full'>
+          <button className={menuItemClass} onClick={handleStop}>
+            기부 중지
+          </button>
+        </li>
+        <li className='w-full'>
+          <button className={`${menuItemClass} text-complementary-alert`} onClick={handleDelete}>
+            기부 삭제
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+};
