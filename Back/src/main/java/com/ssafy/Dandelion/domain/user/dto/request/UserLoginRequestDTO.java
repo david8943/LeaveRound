0327e9cf04ae7d5640ef09c7863e8f2f44024749
juwon <juwon0404@nate.com>
2025-03-24 18:06:01 +0900

    user dto

---
+++ Back/src/main/java/com/ssafy/Dandelion/domain/user/dto/request/UserLoginRequestDTO.java
@@ -0,0 +1,19 @@
+package com.ssafy.Dandelion.domain.user.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class UserLoginRequestDTO {
+
+    @NotBlank(message = "이메일은 필수 입력값입니다.")
+    @Email(message = "유효하지 않은 이메일 형식입니다.")
+    private String email;
+    @NotBlank(message = "비밀번호는 필수 입력 항목입니다.")
+    private String password;
+}
