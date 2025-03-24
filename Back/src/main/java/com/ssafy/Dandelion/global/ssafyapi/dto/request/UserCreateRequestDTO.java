5361b7e8c35a37849bfe944859d46592fe696f96
juwon <juwon0404@nate.com>
2025-03-24 17:30:57 +0900

    변경

--- Back/src/main/java/com/ssafy/Dandelion/global/ssafyapi/dto/request/UserCreateRequestDTO.java
+++ Back/src/main/java/com/ssafy/Dandelion/global/ssafyapi/dto/request/UserCreateRequestDTO.java
@@ -3,10 +3,8 @@ package com.ssafy.Dandelion.global.ssafyapi.dto.request;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class UserCreateRequestDTO {
