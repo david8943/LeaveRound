b2300d2e083e6f35563e203c34a9aeddf3034002
juwon <juwon0404@nate.com>
2025-03-24 16:34:37 +0900

    변경

--- Back/src/main/java/com/ssafy/Dandelion/global/config/RestTemplateConfig.java
+++ Back/src/main/java/com/ssafy/Dandelion/global/config/RestTemplateConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+// 외부 API에 HTTP 요청을 보내고 응답을 받을 때 사용함
 public class RestTemplateConfig {
     @Bean
     public RestTemplate restTemplate() {
