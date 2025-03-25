ce2984f553494679c928c12255138e04cee29ce1
juwon <juwon0404@nate.com>
2025-03-25 13:46:21 +0900

    변경

--- Back/src/main/java/com/ssafy/Dandelion/domain/user/dto/request/UserSignUpRequestDTO.java
+++ Back/src/main/java/com/ssafy/Dandelion/domain/user/dto/request/UserSignUpRequestDTO.java
@@ -6,9 +6,11 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class UserSignUpRequestDTO {
 
--- Back/src/main/java/com/ssafy/Dandelion/global/config/SsafyApiProperties.java
+++ Back/src/main/java/com/ssafy/Dandelion/global/config/SsafyApiProperties.java
@@ -1,4 +1,4 @@
-package com.ssafy.Dandelion.global.ssafyapi.config;
+package com.ssafy.Dandelion.global.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
