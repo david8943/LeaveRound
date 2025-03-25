b8edc0e169c61cd00dd2e5d6dea2d4de287d4e50
juwon <juwon0404@nate.com>
2025-03-25 15:12:48 +0900

    변경

--- Back/src/main/java/com/ssafy/Dandelion/domain/user/service/UserServiceImpl.java
+++ Back/src/main/java/com/ssafy/Dandelion/domain/user/service/UserServiceImpl.java
@@ -1,4 +1,29 @@
 package com.ssafy.Dandelion.domain.user.service;
 
-public class UserServiceImpl {
+import com.ssafy.Dandelion.domain.user.dto.request.UserLoginRequestDTO;
+import com.ssafy.Dandelion.domain.user.dto.request.UserSignUpRequestDTO;
+import com.ssafy.Dandelion.domain.user.dto.response.UserInfoResponseDTO;
+import com.ssafy.Dandelion.domain.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserServiceImpl implements UserService {
+    @Override
+    public void signup(UserSignUpRequestDTO userSignUpRequestDTO) {
+
+    }
+
+    @Override
+    public User authenticate(UserLoginRequestDTO userLoginRequestDTO) {
+        return null;
+    }
+
+    @Override
+    public UserInfoResponseDTO getUserInfo(Integer userId) {
+        return null;
+    }
 }
