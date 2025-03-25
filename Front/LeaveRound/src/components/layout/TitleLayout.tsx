ba8f72c754739c39365636f75f1c5cb9a896fb9e
hajin1015@naver.com <hajin1015@naver.com>
2025-03-25 15:41:16 +0900

    S12P21A303-60-Fix: 레이아웃 수정

--- Front/LeaveRound/src/components/layout/TitleLayout.tsx
+++ Front/LeaveRound/src/components/layout/TitleLayout.tsx
@@ -9,7 +9,7 @@ interface TitleLayoutProps {
 const TitleLayout: React.FC<TitleLayoutProps> = ({ title, children }) => {
   return (
     <div className='h-screen'>
-      <div className='fixed top-0 left-0 right-0 w-full h-[62px] bg-background z-10'>
+      <div className='w-full h-[62px] bg-background z-10'>
         <div className='w-full h-full relative flex items-center'>
           <div className='absolute left-[30px]'>
             <img src={LeftArrow} />
@@ -17,8 +17,7 @@ const TitleLayout: React.FC<TitleLayoutProps> = ({ title, children }) => {
           <div className='w-full text-center font-semibold'>{title}</div>
         </div>
       </div>
-
-      <div className='pt-[62px] h-screen pb-[5rem] overflow-y-auto'>{children}</div>
+      <div className='h-[calc(100%-5rem)] overflow-y-auto'>{children}</div>
     </div>
   );
 };
