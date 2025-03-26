7803926a73b0880eac8e7d9d7708078fc450f7a2
juwon <juwon0404@nate.com>
2025-03-26 09:32:28 +0900

    변경

--- Back/src/main/java/com/ssafy/Dandelion/domain/user/service/UserServiceImpl.java
+++ Back/src/main/java/com/ssafy/Dandelion/domain/user/service/UserServiceImpl.java
@@ -33,15 +33,34 @@ public class UserServiceImpl implements UserService {
      */
     @Override
     @Transactional
+    /**
+     * 회원가입 처리
+     * @param userSignUpRequestDTO 회원가입 요청 정보
+     * @throws MemberHandler 이메일이 이미 존재하는 경우 예외 발생
+     * @throws SsafyApiHandler SSAFY API 연동 실패 시 예외 발생
+     */
     public void signup(UserSignUpRequestDTO userSignUpRequestDTO) {
         // 이메일 중복 체크
         if (userRepository.exitsByEmail(userSignUpRequestDTO.getEmail())) {
             throw new MemberHandler(ErrorStatus.MEMBER_DUPLICATE_EMAIL);
         }
         //SSAFY API를 통해 userKey 발급
+        String userKey = createUser(userSignUpRequestDTO.getEmail());
 
-
+        // 사용자 생성
+        User user = User.builder()
+                .name(userSignUpRequestDTO.getName())
+                .email(userSignUpRequestDTO.getEmail())
+                .password(passwordEncoder.encode(userSignUpRequestDTO.getPassword()))
+                .userKey(userKey)
+                .dandelionCount(0)
+                .dandelionUseCount(0)
+                .goldDandelionCount(0)
+                .goldDandelionUseCount(0)
+                .build();
+        userRepository.save(user);
     }
+
 
     // 로그인
     @Override
@@ -54,4 +73,18 @@ public class UserServiceImpl implements UserService {
     public UserInfoResponseDTO getUserInfo(Integer userId) {
         return null;
     }
+
+
+    /**
+     * SSAFY API를 호출하여 사용자 생성 및 userKey 발급받기
+     *
+     * @param email 사용자 이메일
+     * @return 발급받은 userKey
+     * @throws SsafyApiHandler API 호출 실패 시 예외 발생
+     */
+    private String createUser(String email) {
+
+    }
+
+
 }
