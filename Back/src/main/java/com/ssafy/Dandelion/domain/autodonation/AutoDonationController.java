fd11a3cef676b01804c370d950dff2c4717c0a40
ranunclulus <morion002@gmail.com>
2025-03-26 09:32:02 +0900

    S12P21A303-69-feat: 자동 기부 수정 API 작성

--- Back/src/main/java/com/ssafy/Dandelion/domain/autodonation/AutoDonationController.java
+++ Back/src/main/java/com/ssafy/Dandelion/domain/autodonation/AutoDonationController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,7 +30,7 @@ public class AutoDonationController {
 	@PostMapping("")
 	public ApiResponse<Void> createAutoDonation(
 		@AuthenticationPrincipal CustomUserDetails customUserDetails,
-		@RequestBody @Valid RequestDTO.CreateAutoDonationDTO request
+		@RequestBody @Valid RequestDTO.AutoDonationDTO request
 	) {
 		//autoDonationService.createAutoDonation(customUserDetails.getUserId(), request);
 		autoDonationService.createAutoDonation(1, request);
@@ -61,6 +62,16 @@ public class AutoDonationController {
 	) {
 		//autoDonationService.deleteAutoDonation(customUserDetails.getUserId(), autoDonationId);
 		autoDonationService.deleteAutoDonation(1, autoDonationId);
+		return ApiResponse.onSuccess(null);
+	}
+
+	@PutMapping("/{autoDonationId}")
+	public ApiResponse<Void> UpdateAutoDonation(
+		@AuthenticationPrincipal CustomUserDetails customUserDetails,
+		@PathVariable Integer autoDonationId,
+		@RequestBody @Valid RequestDTO.AutoDonationDTO request
+	) {
+		autoDonationService.UpdateAutoDonation(1, autoDonationId, request);
 		return ApiResponse.onSuccess(null);
 	}
 }
--- Back/src/main/java/com/ssafy/Dandelion/domain/autodonation/service/AutoDonationService.java
+++ Back/src/main/java/com/ssafy/Dandelion/domain/autodonation/service/AutoDonationService.java
@@ -4,11 +4,13 @@ import com.ssafy.Dandelion.domain.autodonation.dto.RequestDTO;
 import com.ssafy.Dandelion.domain.autodonation.dto.ResponseDTO;
 
 public interface AutoDonationService {
-	void createAutoDonation(Integer userId, RequestDTO.CreateAutoDonationDTO request);
+	void createAutoDonation(Integer userId, RequestDTO.AutoDonationDTO request);
 
 	ResponseDTO.ReadAllAutoDonationDTO readAllAutoDonation(Integer userId);
 
 	void changeActive(Integer userId, Integer autoDonationId);
 
 	void deleteAutoDonation(Integer userId, Integer autoDonationId);
+
+	void UpdateAutoDonation(Integer userId, Integer autoDonationId, RequestDTO.AutoDonationDTO request);
 }
