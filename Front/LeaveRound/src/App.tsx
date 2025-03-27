ae3df237635d7bdeaf2bbc9bce202c1cac88abb6
david8943 <david8943@naver.com>
2025-03-27 11:10:47 +0900

    Feat: 로그인 페이지 구현

--- Front/LeaveRound/src/App.tsx
+++ Front/LeaveRound/src/App.tsx
@@ -3,12 +3,13 @@ import { Route, Routes, useLocation } from 'react-router-dom';
 import { AccountDonate } from './pages/AccountDonatePage.tsx';
 import { AccountDetail } from './pages/AccountDetailPage.tsx';
 import Onboarding from './pages/OnboardingPage.tsx';
+import Login from './pages/LoginPage.tsx';
 
 function App() {
   const location = useLocation();
 
   // MenuBar를 숨길 경로들
-  const hideMenuBarPaths = ['/'];
+  const hideMenuBarPaths = ['/', '/login'];
 
   // 현재 경로가 해당 배열에 포함되는지 확인
   const shouldShowMenuBar = !hideMenuBarPaths.includes(location.pathname);
@@ -19,6 +20,7 @@ function App() {
         <main className='flex-1 pb-[5rem]'>
           <Routes>
             <Route path='/' element={<Onboarding />} />
+            <Route path='/login' element={<Login />} />
             <Route path='/:userId/donate' element={<AccountDonate />} />
             <Route path='/:userId/donate/:id' element={<AccountDetail />} />
           </Routes>
--- Front/LeaveRound/src/pages/OnboardingPage.tsx
+++ Front/LeaveRound/src/pages/OnboardingPage.tsx
@@ -10,70 +10,70 @@ import Card2 from "@/assets/images/OnboardingCard2.svg";
 import Card3 from "@/assets/images/OnboardingCard3.svg";
 
 const Onboarding: React.FC = () => {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    centerMode: true,
-    // centerPadding: "0px",
-  };
-  
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        // centerMode: true,
+        // centerPadding: "0px",
+    };
 
-  return (
-    <div className="bg-background flex flex-col items-center w-[var(--width-base)] mx-auto">
-      <img
-        src={LeaveRound}
-        alt="Leave Round"
-        className="w-auto h-auto relative -top-[90px] left-[15px]"
-      />
 
-      {/* 텍스트 */}
-      <p className="mt-[-170px] text-detail">
-        리브라운드와 함께 선한 영향력을 전달해보세요.
-      </p>
-
-      {/* 캐러셀 */}
-      <Slider {...settings} className="w-full">
-        {[Card1, Card2, Card3].map((card, index) => (
-          <div key={index}>
+    return (
+        <div className="bg-background flex flex-col items-center w-[var(--width-base)] mx-auto">
             <img
-              src={card}
-              alt={`Card ${index + 1}`}
-              className="mx-auto w-full"
+                src={LeaveRound}
+                alt="Leave Round"
+                className="w-auto h-auto relative -top-[90px] left-[15px]"
             />
-          </div>
-        ))}
-      </Slider>
+
+            {/* 텍스트 */}
+            <p className="mt-[-170px] text-detail">
+                리브라운드와 함께 선한 영향력을 전달해보세요.
+            </p>
+
+            {/* 캐러셀 */}
+            <Slider {...settings} className="w-full">
+                {[Card1, Card2, Card3].map((card, index) => (
+                    <div key={index}>
+                        <img
+                            src={card}
+                            alt={`Card ${index + 1}`}
+                            className="mx-auto w-full"
+                        />
+                    </div>
+                ))}
+            </Slider>
 
 
 
-      {/* 로그인 버튼 */}
-      <div className="absolute bottom-[95px]">
-        <button
-        className="w-[313px] h-[48px] bg-[var(--primary)] text-white rounded-[24px] text-base font-medium disabled:bg-[var(--text-disabled)]"
-        >
-        로그인
-        </button>
-      </div>
+            {/* 로그인 버튼 */}
+            <div className="absolute bottom-[95px]">
+                <button
+                    className="w-[313px] h-[48px] bg-[var(--primary)] text-white rounded-[24px] text-base font-medium disabled:bg-[var(--text-disabled)]"
+                >
+                    로그인
+                </button>
+            </div>
 
-      {/* 회원가입 링크 */}
-      <p className="absolute bottom-[53px] text-detail">
-        아직 회원이 아니신가요?{" "}
-        <span
-          className="text-primary cursor-pointer"
-          onClick={() => navigate("/signup")}
-        >
-          회원가입
-        </span>
-      </p>
-    </div>
-  );
+            {/* 회원가입 링크 */}
+            <p className="absolute bottom-[53px] text-detail">
+                아직 회원이 아니신가요?{" "}
+                <span
+                    className="text-primary cursor-pointer"
+                    onClick={() => navigate("/signup")}
+                >
+                    회원가입
+                </span>
+            </p>
+        </div>
+    );
 };
 
 export default Onboarding;
