d0e7db9f3088b282ce56bed85f2807dbafc0b9e4
hajin1015@naver.com <hajin1015@naver.com>
2025-04-01 13:58:26 +0900

    S12P21A303-91-Feat: api url 추가

---
+++ Front/LeaveRound/src/constants/url.ts
@@ -0,0 +1,27 @@
+const API_VERSION: string = '/api';
+
+export const API = {
+  member: {
+    signup: `${API_VERSION}/users`,
+    login: `${API_VERSION}/users/login`,
+    info: (userId: string) => `${API_VERSION}/users/${userId}`,
+    account: (userId: string) => `${API_VERSION}/users/${userId}/acounts`,
+  },
+  event: {
+    getYellowDandelion: (dandelionId: string) => `${API_VERSION}/dandelions/collections/gold/${dandelionId}`,
+    getWhiteDandelion: (dandelionId: string) => `${API_VERSION}/dandelions/collections/personal/${dandelionId}`,
+    whiteLocation: `${API_VERSION}/daldelions/locations/personal`,
+    yellowLocation: `${API_VERSION}/dandelions/locations/gold `,
+    donateDandelion: `${API_VERSION}/dandelions/donations/organizations`,
+    whiteDandelionRanking: `${API_VERSION}/dandelions/donations/rankings/weekly`,
+    yellowDandelionRanking: `${API_VERSION}//dandelions/donations/rankings/gold/monthly`,
+  },
+  autoDonation: {
+    detail: (autoDonationId: string) => `${API_VERSION}/auto-donations/${autoDonationId}`,
+  },
+  organization: {
+    list: `${API_VERSION}/organizations`,
+    project: (organizationId: string) => `${API_VERSION}/organizations/${organizationId}/project`,
+    certification: (certificateId: string) => `${API_VERSION}/organizations/${certificateId}`,
+  },
+};
