58f99713d34d4a017d368c43f8809b6060be9e0e
ranunclulus <morion002@gmail.com>
2025-04-01 15:24:03 +0900

    S12P21A303-92-fix: Donation Time 변경

--- Back/src/main/java/com/ssafy/Dandelion/domain/autodonation/entity/constant/DonationTime.java
+++ Back/src/main/java/com/ssafy/Dandelion/domain/autodonation/entity/constant/DonationTime.java
@@ -1,5 +1,15 @@
 package com.ssafy.Dandelion.domain.autodonation.entity.constant;
 
 public enum DonationTime {
-	ONE_DAY, TWO_DAY, THREE_DAY, FOUR_DAY, FIVE_DAY, SIX_DAY, SEVEN_DAY;
+	ONE_DAY(1), TWO_DAY(2), THREE_DAY(3), FOUR_DAY(4), FIVE_DAY(5), SIX_DAY(6), SEVEN_DAY(7);
+
+	private final int days;
+
+	DonationTime(int days) {
+		this.days = days;
+	}
+
+	public int getDays() {
+		return days;
+	}
 }
