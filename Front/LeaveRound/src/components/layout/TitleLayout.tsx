2fb6781c2c1c8ca018b7c0a71758fc812fbcdb59
hajin1015@naver.com <hajin1015@naver.com>
2025-04-01 17:00:20 +0900

    테스트 중!!!!!!!!!!!!!!

--- Front/LeaveRound/src/components/layout/TitleLayout.tsx
+++ Front/LeaveRound/src/components/layout/TitleLayout.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from 'react';
-import LeftArrow from '../../assets/icons/left-arrow.svg';
+import LeftArrow from '@/assets/icons/left-arrow.svg';
+import { useNavigate } from 'react-router-dom'; // ✅ 추가
 
 interface TitleLayoutProps {
   title: string;
@@ -7,11 +8,17 @@ interface TitleLayoutProps {
 }
 
 const TitleLayout: React.FC<TitleLayoutProps> = ({ title, children }) => {
+  const navigate = useNavigate(); // ✅ 훅 사용
+
+  const handleBack = () => {
+    navigate(-1); // ✅ 뒤로가기
+  };
+
   return (
     <div className='h-screen'>
       <div className='w-full h-[62px] bg-background z-10'>
         <div className='w-full h-full relative flex items-center'>
-          <div className='absolute left-[30px]'>
+          <div className='absolute left-[30px]' onClick={handleBack}>
             <img src={LeftArrow} />
           </div>
           <div className='w-full text-center font-semibold'>{title}</div>
