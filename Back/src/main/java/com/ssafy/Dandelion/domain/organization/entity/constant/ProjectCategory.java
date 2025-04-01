c0dd3605a14751d5c3ee26921097d097ea484558
TinyFrogs <qns0147@gmail.com>
2025-04-01 15:20:21 +0900

    S12P21A303-90-Feat: 이름에 매핑되는 ProjectCategory 메소드 구현

--- Back/src/main/java/com/ssafy/Dandelion/domain/organization/entity/constant/ProjectCategory.java
+++ Back/src/main/java/com/ssafy/Dandelion/domain/organization/entity/constant/ProjectCategory.java
@@ -13,4 +13,13 @@ public enum ProjectCategory {
 	OTHER("기타");
 
 	private final String categoryName;
+
+	public static ProjectCategory fromCategoryName(String categoryName) {
+		for (ProjectCategory category : ProjectCategory.values()) {
+			if (category.categoryName.equals(categoryName)) {
+				return category;
+			}
+		}
+		throw new IllegalArgumentException("Unknown category name: " + categoryName);
+	}
 }
