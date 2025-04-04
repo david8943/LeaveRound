256a873a6114c515f8e058def612b28395b7c268
hajin1015@naver.com <hajin1015@naver.com>
2025-04-04 11:03:58 +0900

    S12P21A303-104-Feat: 민들레 주간 랭킹 api 연결

---
+++ Front/LeaveRound/src/models/ranking.ts
@@ -0,0 +1,11 @@
+export interface TRankInfo {
+  rank: number;
+  name: string;
+  donateCount: number;
+}
+
+export interface TMyRankInfo {
+  myRank: number;
+  myName: string;
+  myDonateCount: number;
+}
