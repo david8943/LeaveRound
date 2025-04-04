155cca4348047da978a93b2ecf90e8ea2cd32a19
juwon <juwon0404@nate.com>
2025-04-04 16:11:39 +0900

    S12P21A303-100-10-dandelionCount

--- Back/src/main/java/com/ssafy/Dandelion/domain/user/dto/response/UserInfoResponseDTO.java
+++ Back/src/main/java/com/ssafy/Dandelion/domain/user/dto/response/UserInfoResponseDTO.java
@@ -1,42 +1,43 @@
 package com.ssafy.Dandelion.domain.user.dto.response;
 
+import java.time.format.DateTimeFormatter;
+
 import com.ssafy.Dandelion.domain.user.entity.User;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.format.DateTimeFormatter;
 
 @Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserInfoResponseDTO {
-    private Integer userId;
-    private String name;
-    private String email;
-    private String userKey;
-    private int dandelionCount;
-    private int goldDandelionCount;
-    private int totalDonationCount;
-    private String createdAt;
-    private String updatedAt;
+	private Integer userId;
+	private String name;
+	private String email;
+	private String userKey;
+	private int dandelionCount;
+	private int goldDandelionCount;
+	private int totalDonationCount;
+	private String createdAt;
+	private String updatedAt;
 
-    // User 엔터티로부터 DTO 생성을 위한 정적 팩토리 메서드
-    public static UserInfoResponseDTO fromEntity(User user) {
-        DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
+	// User 엔터티로부터 DTO 생성을 위한 정적 팩토리 메서드
+	public static UserInfoResponseDTO fromEntity(User user) {
+		DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
 
-        return UserInfoResponseDTO.builder()
-                .userId(user.getUserId())
-                .name(user.getName())
-                .email(user.getEmail())
-                .userKey(user.getUserKey())
-                .dandelionCount(user.getDandelionCount())
-                .goldDandelionCount(user.getGoldDandelionCount())
-                .totaldonationCount(user.getDandelionUseCount() + (user.getGoldDandelionUseCount() * 100))
-                .createdAt(user.getCreatedAt().format(formatter))
-                .updatedAt(user.getUpdatedAt().format(formatter))
-                .build();
-    }
+		return UserInfoResponseDTO.builder()
+			.userId(user.getUserId())
+			.name(user.getName())
+			.email(user.getEmail())
+			.userKey(user.getUserKey())
+			.dandelionCount(user.getDandelionCount())
+			.goldDandelionCount(user.getGoldDandelionCount())
+			.totalDonationCount(user.getDandelionUseCount() + (user.getGoldDandelionUseCount() * 100))
+			.createdAt(user.getCreatedAt().format(formatter))
+			.updatedAt(user.getUpdatedAt().format(formatter))
+			.build();
+	}
 }
